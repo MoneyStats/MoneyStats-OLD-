cfb5e6fe36d89db2ca1c9afaeaed4c030287ee91
giovannilamarmora <giovannilamarmora.working@gmail.com>
2021-06-04 22:31:41 +0200

    Added changelog.html

--- src/main/java/com/moneystats/MoneyStats/controller/ControllerCategory.java
+++ src/main/java/com/moneystats/MoneyStats/controller/ControllerCategory.java
@@ -4,17 +4,19 @@ import com.moneystats.MoneyStats.model.Category;
 import com.moneystats.MoneyStats.repositoryCRUD.ICategoryCRUD;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/category")
 public class ControllerCategory {
 
     @Autowired
     ICategoryCRUD categoryGEST;
 
-    @GetMapping("/category")
+    @GetMapping("/list")
     public List<Category> categoryList() {
         return categoryGEST.findAll();
     }
---
+++ src/main/resources/static/js/wallet.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    function getWallet(){
+        $.get('/wallet/list', function (resume) {
+            console.log(resume);
+            const listWallet = $('#listWallet');
+            for (let i = resume.length - 1; i >= 0; i--) {
+                $(`<tr id='riga-${resume[i].id}'>
+                <td>${resume[i].name}</td>
+                <td>${resume[i].category.name}</td>
+                <td>
+                    <div class="btn-group roundedCorner" role="group">
+                        <button id="btnGroupDrop1" type="button" class="btn btn-outline-warning roundedCorner dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Opzioni</button>
+                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                <li><a class="dropdown-item btn-dettaglio" data-bs-toggle="modal" data-bs-target="#dettaglio" data-id='${resume[i].id}'>Dettaglio</a></li>
+                                <li><a class="dropdown-item btn-modifica-risto" data-bs-toggle="modal" data-bs-target="#modifica" data-id='${resume[i].id}'>Modifica</a></li>
+                                <li><a class="dropdown-item btn-elimina-risto" data-id='${resume[i].id}'>Elimina</a></li>
+                            </ul>
+                    </div>
+                </td>
+            </tr>`).hide().appendTo(listWallet).fadeIn(i * 20);
+            }
+        })
+    }
+    getWallet();
+});
