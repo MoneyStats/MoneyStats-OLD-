22069ba250d51f626887b9a2787a428cc221a124
giovannilamarmora <giovannilamarmora.working@gmail.com>
2021-06-09 00:32:13 +0200

    Bug fixed, Connected homepage.html

--- src/main/resources/static/js/statement.js
+++ src/main/resources/static/js/statement.js
@@ -4,30 +4,41 @@ $(document).ready(function () {
             const listWallet = $('#listWallet');
             for (let i = 0; i < resume.length; i++) {
                 document.cookie = "id"+i+" = " + resume[i].id;
-                $(`<div class="col"}>
-                <div class="card h-100">
-                  <img style="background-size: cover; " src="https://blog.revolut.com/content/images/2018/05/Revolut-Metal-Card---Blue-2.png" class="card-img h-100" alt="...">
-                  <div class="card-img-overlay">
-                  <div class="card h-100" style="background-color: rgba(255, 255, 255, 0.7);">
+                $(`<div class="col-xl-3 col-sm-6" style="margin-bottom: 30px;">
+                <div class="card card-stats mb-4 mb-xl-0">
                   <div class="card-body">
-                    <h5 class="card-title">Portafoglio ${resume[i].name}</h5>
-                    <input type='hidden' id="wallet${resume[i].id}" value="${resume[i].id}">
-                    <p class="card-text">Inserire quantitativo in £GBP</p>
-                    <div class='form-floating mb-3'> 
+                  <div class="row">
+                      <div class="col">
+                      <h5 class="card-title text-uppercase text-muted mb-0">${resume[i].name} (${resume[i].category.name})</h5>
+                      <div class='form-floating mb-3'> 
                       <input style="background-color: rgba(255, 255, 255, 0.7);" type='number' id="value${resume[i].id}" class='form-control roundedCorner mx-auto' placeholder="Inserire Valore in £GBP..." required>
                       <label for='value'>Inserire Valore in £GBP</label>
                   </div>
-                  </div>
-                  </div>
-                </div>
-                </div>
-              </div>`).hide().appendTo(listWallet).fadeIn(i * 20);
+                      <input type='hidden' id="wallet${resume[i].id}" value="${resume[i].id}">
+                      </div>
+                      <div class="col-auto">
+                      <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                          <i class="fas fa-chart-bar"></i>
+                          </div>
+                          </div>
+                    </div>
+                    <p class="mt-3 mb-0 text-muted text-sm">
+                    <span class="text-nowrap sincetot">Inserire Valori</span>
+                      </p>
+                      </div>
+                `).hide().appendTo(listWallet).fadeIn(i * 20);
             }
-        })
-    }
-    getWallet();
-
-    $('#aggiungiStatement').click(function () {
+          })
+        }
+        getWallet();
+        
+      $('#aggiungiStatement').click(function () {
+        var cookie = document.cookie;
+        cookiearray = cookie.split(';');
+        for (let i = 0; i < cookiearray.length; i++){
+          valueCookie = cookiearray[i].split('=')[1];
+          $(`#value${valueCookie}`).prop('required', true);
+        }
         Swal.fire({
             title: 'Vuoi salvare lo statement?',
             showDenyButton: true,
@@ -37,8 +48,7 @@ $(document).ready(function () {
             icon: 'question',
           }).then((result) => {
             if (result.isConfirmed) {
-                var cookie = document.cookie;
-                cookiearray = cookie.split(';');
+                
                 console.log(cookiearray);
                 for (let i = 0; i < cookiearray.length; i++){
                     console.log("key is" + cookiearray[i]);
